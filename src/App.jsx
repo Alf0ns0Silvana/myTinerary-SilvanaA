134c7839adb02dcbe0ceecc8e7a1d6aac0eeb309
Silvana A <alfonsosilvana16@gmail.com>
2023-08-01 15:27:44 -0300

    iniciando myTinerary

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Vector from '/Vector.png'
 import './App.css'
 
 function App() {
@@ -8,26 +7,21 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <header className='header'>
+      <h1>My Tinerary</h1>
+          <nav className='nav'>
+          <a href="">Home</a>
+          <a href="">Cities</a>
+          <button className='login_btn'> <img src={Vector} alt="Icon login" />Login</button>
+          </nav>
+      </header>
+      <main>
+      </main>
+      <footer className="card">
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Example
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </footer>
     </>
   )
 }
