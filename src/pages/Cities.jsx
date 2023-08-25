b8e34fc5c9da8604cc31fda88f87a6b7f59aa24c
Silvana A <alfonsosilvana16@gmail.com>
2023-08-25 12:09:54 -0300

    manejandoInputSearch&notFound

--- src/pages/Cities.jsx
+++ src/pages/Cities.jsx
@@ -1,38 +1,53 @@
 import React from 'react'
 import Cardcities from '../components/Cardcities'
 import axios from 'axios'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import {Link} from 'react-router-dom'
 
 const Cities = () => {
     const [cities, setCities] = useState([]);
+
+    let inputSearch = useRef(); 
 
     useEffect (() => {
       axios.get('http://localhost:8000/api/cities?name=')
         .then(response => setCities(response.data.cities))
         .catch(err => console.log(err))
     }, []);
-    
-    const handleInputChange = async (city) => {
-      console.log(city.target.value) 
+
+    const handleSearch = async () => {
+
+      console.log(inputSearch.current.value)
+        const name = inputSearch.current.value
+
         try {
-          const response = await axios.get( `http://localhost:8000/api/cities?name=${city.target.value}`) // este reeemplaza .then
+          const response = await axios.get( `http://localhost:8000/api/cities?name=${name}`)
           setCities(response.data.cities)
         } catch (error) {
-          console.log(error)
+            if(error.response.status === 404) {
+              console.log('Not found city')
+              setCities([])
+            } else {
+              console.log(error);
+            }
         }
     }
 
     return (
       <div>
         <h2 className='pages-subtitles'>Cities</h2>
-        <input onChange={handleInputChange} className='search' type="text" placeholder='Ex:  kuwuait'/>
+          <div className="search_section">
+            <input ref={inputSearch} className='search' type="text" placeholder='Ex: kuwuait'/>
+            <button onClick={handleSearch} className='btn_search'>Search</button>
+          </div>
         {
-          cities?.map(city => (
-            <Link key={city._id} to={`/cities/${city._id}`}>
-              <Cardcities city={city} />
-            </Link>
-          ))
+          cities?.length > 0
+          ? cities?.map(city => (
+              <Link key={city._id} to={`/cities/${city._id}`}>
+                <Cardcities city={city} />
+              </Link>
+            )) 
+            : <h3 className='msj_not_found'>Sorry, we don't have information about that city 😞 </h3>
         }
       </div>
   )
